6dbcb5ff3a34dd8c1522ce113b3555eb68dbfc44
Alexey Anshakov <a.a@tutanota.com>
2023-06-23 13:24:09 +0200

    test

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,10 +1,12 @@
 const airtableBaseId = 'app166IA16JVxqLL4';
 const airtableTable = 'Users';
+const fieldName = 'Email';
+const criteria = 'a.a@tutanota.com';
 
-fetch(`https://airtable.wrio.workers.dev/?baseId=${airtableBaseId}&table=${airtableTable}`)
+fetch(`https://airtable.wrio.workers.dev/?baseId=${airtableBaseId}&table=${airtableTable}&field=${fieldName}&criteria=${criteria}`)
   .then(response => response.json())
   .then(data => {
-    // Process the data returned by the Cloudflare Worker
+    // Process the filtered data returned by the Cloudflare Worker
     console.log(data);
   })
   .catch(error => {
