c52aa1cf2598d211ba77783c96fb5c992602d8a0
Alexey Anshakov <a.a@tutanota.com>
2023-07-09 14:46:21 +0200

    addons upd

--- addons/TwitterFollowersCount/script.js
+++ addons/TwitterFollowersCount/script.js
@@ -2,12 +2,11 @@
 window.addEventListener('load', () => {
   const username = "alexey_anshakov";
   let retryCount = 0;
-  const maxRetries = 10;
+  const maxRetries = 5;
 
   const fetchFollowerCount = () => {
     // Scrape the follower count
-    //const followerCountElement = document.querySelector(`a[href="/${username}/followers"] span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0 span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0`);
-    const followerCountElement = document.querySelector('li.text-body-small.t-black--light.inline-block span.t-bold');
+    const followerCountElement = document.querySelector('span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
     const followerCount = followerCountElement ? followerCountElement.textContent : null;
 
     if (followerCount) {
@@ -19,7 +18,7 @@ window.addEventListener('load', () => {
         retryCount++;
         setTimeout(fetchFollowerCount, 1000);
       } else {
-        console.log('Unable to fetch follower count.');
+        console.log('Unable to fetch follower count!');
       }
     }
   };
